a45780039831dc9ebde7248c544f0a9a07d054b3
Cécile Guillot <64648386+Sylvariane@users.noreply.github.com>
2021-07-23 23:59:20 +0200

    correction notebook modélisation, maj model définitif+pred

--- Pélec_03_model.py
+++ Pélec_03_model.py
@@ -14,31 +14,29 @@ from sklearn.impute import SimpleImputer
 from sklearn.model_selection import  train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
 import joblib
 
 # loading the dataset
 data = pd.read_csv("datasets/benchmark_total.csv")
 
-# Separation of the features and the two targets
-y_energy = data["SiteEnergyUse(kBtu)"].values
-y_ghg = data["TotalGHGEmissions"].values
-X = data.drop(["SiteEnergyUse(kBtu)", "TotalGHGEmissions", "ENERGYSTARScore", "NbofFloors", "NbofBuildings"], axis=1)
-
 #######################
 ######ENERGY USE#######
 #######################
 
-# Preparation of the different datasets
-X_train, X_test, y_train, y_test = train_test_split(X, y_energy, 
-                                                    test_size=0.2, random_state=42)
-y_train = y_train.reshape(-1,1)
-y_test = y_test.reshape(-1,1)
+train_set, test_set = train_test_split(data, test_size=0.2, random_state=42, stratify=data["Clusters"])
 
+y_train = train_set["SiteEnergyUse(kBtu)"].values
+y_test = test_set["SiteEnergyUse(kBtu)"].values
+X_train = train_set.drop(["SiteEnergyUse(kBtu)", "TotalGHGEmissions", "ENERGYSTARScore", "Age", "YearBuilt", "Neighborhood", "NumberofBuildings", "NumberofFloors", "Latitude", "Longitude"], axis=1)
+X_test = test_set.drop(["SiteEnergyUse(kBtu)", "TotalGHGEmissions", "ENERGYSTARScore", "Age", "YearBuilt", "Neighborhood", "NumberofBuildings", "NumberofFloors","Latitude", "Longitude"], axis=1)
+
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
 
 # Preprocessing of categorical and numerical variables
-cat_var = ["PrimaryPropertyType", "Neighborhood"]
-num_var = ["YearBuilt", "NumberofBuildings", "NumberofFloors", "PropertyGFATotal"]
+cat_var = ["PrimaryPropertyType", "NbofFloors", "NbofBuildings", "HasParking", "Clusters", "Bins_Age"]
+num_var = ["PropertyGFATotal", "degreeDaysH", "degreeDaysC"]
 
 cat_pipe = Pipeline([
     ('imputer', SimpleImputer(strategy='most_frequent', fill_value='missing')),
@@ -58,14 +56,15 @@ X_test = preprocessor.transform(X_test)
 
 
 # Preparation of the model
-forest_reg_energy = RandomForestRegressor(n_estimators=425, min_samples_leaf=3, max_depth=4,
-                                   min_samples_split=2, max_features="auto", random_state=42)
-forest_reg_energy.fit(X_train, y_train)
+xgboost_energy = xgb.XGBRegressor(objective="reg:squarederror", booster="gbtree", 
+                                    gamma=1, learning_rate=0.15, max_depth=12, sampling_method="uniform",
+                                    n_estimators=400, tree_method="hist", random_state=42, n_jobs=-1)
+xgboost_energy.fit(X_train, y_train)
 
 # Final pipeline
 full_pipeline_energy = Pipeline([
     ("preprocessing", preprocessor),
-    ("model", forest_reg_energy)
+    ("model", xgboost_energy)
 ])
 joblib.dump(full_pipeline_energy, 'API_deploiement/models/model_prediction_energy.pkl')
 
@@ -74,33 +73,45 @@ joblib.dump(full_pipeline_energy, 'API_deploiement/models/model_prediction_energ
 #######################
 
 # Preparation of the different datasets
-X_train, X_test, y_train, y_test = train_test_split(X, y_ghg, test_size=0.2, random_state=42)
-y_train = y_train.reshape(-1,1)
-y_test = y_test.reshape(-1,1)
+train_set, test_set = train_test_split(data, test_size=0.2, random_state=42, stratify=data["Clusters"])
+
+y_train = train_set["TotalGHGEmissions"].values
+y_test = test_set["TotalGHGEmissions"].values
+X_train = train_set.drop(["SiteEnergyUse(kBtu)", "TotalGHGEmissions", "ENERGYSTARScore", "YearBuilt", "Age", "Neighborhood", "NumberofBuildings", "NumberofFloors", "Latitude", "Longitude"], axis=1)
+X_test = test_set.drop(["SiteEnergyUse(kBtu)", "TotalGHGEmissions", "ENERGYSTARScore", "YearBuilt", "Age", 'Neighborhood', "NumberofBuildings", "NumberofFloors", "Latitude", "Longitude"], axis=1)
+
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
 
 # Preprocessing of the data
 X_train = preprocessor.transform(X_train)
 X_test = preprocessor.transform(X_test)
 
 # Preparation of the 2nd model
-forest_reg_co2 = RandomForestRegressor(n_estimators=430, min_samples_leaf=2, max_depth=4,
-                                       min_samples_split=2, max_features="auto", random_state=42)
-forest_reg_co2.fit(X_train, y_train)
+xgboost_co2 = xgb.XGBRegressor(objective="reg:squarederror", booster="gbtree", 
+                               gamma=1, learning_rate=0.1, max_depth=12, sampling_method="uniform",
+                               n_estimators = 400, tree_method="hist", random_state=42, n_jobs=-1)
+
+xgboost_co2.fit(X_train, y_train)
 
 # Final pipeline
 full_pipeline_co2 = Pipeline([
     ("preprocessing", preprocessor),
-    ("model", forest_reg_co2)
+    ("model", xgboost_co2)
 ])
 joblib.dump(full_pipeline_co2, "API_deploiement/models/model_prediction_co2.pkl")
 
 # Test
 new_building = pd.DataFrame({"PrimaryPropertyType": "Hospital",
-                             "Neighborhood" : "Delridge",
-                             "YearBuilt" : 2016,
-                             "NumberofBuildings": 5,
-                             "NumberofFloors": 5, 
-                             "PropertyGFATotal": 56700}, index=[1])
+                             "PropertyGFATotal": 56700,
+                             "NbofFloors": "2+f",
+                             "NbofBuildings" : "2+b",
+                             "Bins_Age" : "Recent",
+                             "degreeDaysH" : 4438,
+                             "degreeDaysC" : 247,
+                             "HasParking" : "Yes",
+                             "Clusters" : "2"}, 
+                             index=[1])
 print(new_building)
 print("Estimation de la consommation d'énergie :" + str((full_pipeline_energy.predict(new_building)).round(2)) + " kBtu")
 print("Estimation des émissions de CO2 :" + str((full_pipeline_co2.predict(new_building)).round(2)) + " MetricTonsCO2e")
